b62ea9555ade3676141cca669eb45d8da8ad0275
gsprasannabalaji <govindarajalusaras.p@northeastern.edu>
2024-08-25 18:53:43 -0400

    moved the secret key

--- firebase.js
+++ firebase.js
@@ -8,7 +8,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCi7rN0-i9In9WmQQLgALtUwY4BriZimHA",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "flashcardsass.firebaseapp.com",
   projectId: "flashcardsass",
   storageBucket: "flashcardsass.appspot.com",
